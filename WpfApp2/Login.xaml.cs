107fc18dbea5b01c01d0770147a4934857a7720a
英 刘 <1985029495@qq.com>
2021-01-12 23:22:59 +0800

    考试界面设计

--- WpfApp2/Login.xaml.cs
+++ WpfApp2/Login.xaml.cs
@@ -31,5 +31,6 @@ namespace WpfApp2
             testpage.Show();
             this.Close();
         }
+
     }
 }
--- WpfApp2/TestPage.xaml.cs
+++ WpfApp2/TestPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp2
 {
@@ -19,9 +20,67 @@ namespace WpfApp2
     /// </summary>
     public partial class TestPage : Window
     {
+        private DispatcherTimer timer;
+        private ProcessCount processCount;
+
         public TestPage()
         {
             InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWin_Loaded);
+        }
+
+        /// <summary>
+        /// 窗口加载事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            //设置定时器
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(10000000); //时间间隔为一秒
+            timer.Tick += new EventHandler(timer_Tick);
+            //转换成秒数
+            Int32 hour = Convert.ToInt32(HourArea.Text);
+            Int32 minute = Convert.ToInt32(MinuteArea.Text);
+            Int32 second = Convert.ToInt32(SecondArea.Text);
+            //处理倒计时的类
+            processCount = new ProcessCount(hour * 3600 + minute * 60 + second);
+            CountDown += new CountDownHandler(processCount.ProcessCountDown);
+            //开启定时器
+            timer.Start();
+        }
+        /// <summary>
+        /// Timer触发的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (OnCountDown())
+            {
+                HourArea.Text = processCount.GetHour();
+                MinuteArea.Text = processCount.GetMinute();
+                SecondArea.Text = processCount.GetSecond();
+            }
+            else
+                timer.Stop();
+        }
+        /// <summary>
+        /// 处理事件
+        /// </summary>
+        public event CountDownHandler CountDown;
+        public bool OnCountDown()
+        {
+            if (CountDown != null)
+                return CountDown();
+            return false;
         }
     }
+    /// <summary>
+    /// 处理倒计时的委托
+    /// </summary>
+    /// <returns></returns>
+    public delegate bool CountDownHandler();
 }
+
